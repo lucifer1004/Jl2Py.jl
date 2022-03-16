d4e29fb9b1981801ae237fdd2c6b6b80bb5d086c
Documenter.jl <documenter@juliadocs.github.io>
2022-03-16 14:35:20 +0000

    build based on 62733c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Jl2Py","category":"page"},{"location":"#Jl2Py","page":"Home","title":"Jl2Py","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Jl2Py.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Jl2Py]","category":"page"}]
+}
